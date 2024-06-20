2518e4f6f267ad14d81ba58556ebdca10caf5ec8
libx266 <pies_popierdolony@mail.ru>
2024-06-20 16:28:19 +0400

    update default roles

---
+++ KoboldTgBot/Migrations/20240620122744_UpdsteDefaultRoles2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KoboldTgBot.Migrations
+{
+    public partial class UpdsteDefaultRoles2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "InsertDate", "Title" },
+                values: new object[] { new DateTime(2024, 6, 20, 12, 27, 44, 413, DateTimeKind.Utc).AddTicks(9602), "Эксперт" });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "InsertDate",
+                value: new DateTime(2024, 6, 20, 12, 27, 44, 413, DateTimeKind.Utc).AddTicks(9604));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "ID",
+                keyValue: 3,
+                columns: new[] { "InsertDate", "Specialisation", "Title" },
+                values: new object[] { new DateTime(2024, 6, 20, 12, 27, 44, 413, DateTimeKind.Utc).AddTicks(9605), "командир Императорской гвардии Роccийской Империи", "Офицер" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "InsertDate", "Title" },
+                values: new object[] { new DateTime(2024, 6, 19, 1, 49, 31, 236, DateTimeKind.Utc).AddTicks(6989), "Офицер по науке" });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "InsertDate",
+                value: new DateTime(2024, 6, 19, 1, 49, 31, 236, DateTimeKind.Utc).AddTicks(6992));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "ID",
+                keyValue: 3,
+                columns: new[] { "InsertDate", "Specialisation", "Title" },
+                values: new object[] { new DateTime(2024, 6, 19, 1, 49, 31, 236, DateTimeKind.Utc).AddTicks(6993), "командир Императорской гвардии Ромуланской Империи", "Командир Императорской гвардии" });
+        }
+    }
+}
